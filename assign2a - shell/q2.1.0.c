420d270303571da09b9b9c2e718e120cef990ab0
Kaushik Mahato <kaushik.mahato@cse.iitkgp.ernet.in>
2015-02-01 11:35:16 +0530

    Error fixed for tokenization only by space

--- assign2a - shell/q2.1.0.c
+++ assign2a - shell/q2.1.0.c
@@ -51,8 +51,8 @@ int main(int argc, char *argv[])
 int processBuiltInCommand(char * cmd, int *comm)
 {
   if( strncmp(cmd, "exit", strlen("exit")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
     if(arg1 != NULL){
       printArgumentError();
       return 0;
@@ -60,8 +60,8 @@ int processBuiltInCommand(char * cmd, int *comm)
     return executeExitCommand();
     }
   else if( strncmp(cmd, "pwd", strlen("pwd")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
     if(arg1 != NULL){
       printArgumentError();
       return 0;
@@ -69,8 +69,9 @@ int processBuiltInCommand(char * cmd, int *comm)
       return executePwdCommand();
     }
   else if( strncmp(cmd, "ls", strlen("ls")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
     if(arg1 != NULL){
       printArgumentError();
       return 0;
@@ -78,20 +79,20 @@ int processBuiltInCommand(char * cmd, int *comm)
       return executeLsCommand();
     }
   else if( strncmp(cmd, "cd", strlen("cd")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
-    char * arg2 = strtok (NULL," ,.-");
-    if(arg2 != NULL){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    if(arg2 != NULL }} arg1 == NULL){
       printArgumentError();
       return 0;
       }
       return executeCdCommand(arg1);
     }
   else if( strncmp(cmd, "cp", strlen("cp")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
-    char * arg2 = strtok (NULL," ,.-");
-    char * arg3 = strtok (NULL," ,.-");
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    char * arg3 = strtok (NULL," ");
     if(arg3 != NULL){
       printArgumentError();
       return 0;
@@ -99,9 +100,9 @@ int processBuiltInCommand(char * cmd, int *comm)
       return executeCpCommand(arg1,arg2);
     }
   else if( strncmp(cmd, "mkdir", strlen("mkdir")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
-    char * arg2 = strtok (NULL," ,.-");
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
     if(arg2 != NULL){
       printArgumentError();
       return 0;
@@ -109,9 +110,9 @@ int processBuiltInCommand(char * cmd, int *comm)
       return executeMkdirCommand(arg1);
     }
   else if( strncmp(cmd, "rmdir", strlen("rmdir")) == 0){
-    char * arg0 = strtok (cmd," ,.-");
-    char * arg1 = strtok (NULL," ,.-");
-    char * arg2 = strtok (NULL," ,.-");
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
     if(arg2 != NULL){
       printArgumentError();
       return 0;
