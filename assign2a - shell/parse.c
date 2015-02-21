1f01471b4933a1d77c37674db9d1c520aca62da0
enlighter <enlighter.1992@gmail.com>
2015-02-21 16:04:04 +0530

    Implemented parse.h

--- assign2a - shell/parse.c
+++ assign2a - shell/parse.c
@@ -1,2 +1,9 @@
 /*######### CONTAINS NECESSARY FUNCTIONS FOR PARSING #########*/
 /*######### PLEASE INCLUDE THE FILE "parse.h" IN THE SAME FOLDER #########*/
+
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "parse.h"
+
--- assign2a - shell/parse.h
+++ assign2a - shell/parse.h
@@ -1,2 +1,13 @@
 /*######### HEADER FILE FOR "PARSE" INSTRUCTIONS (parse.c) #########*/
 /*######### CONTAINS NECESSARY PREPROCESSOR AND FUNCTION DECLARATIONS #########*/
+#define	MAX_ARG_NUM	5				//the maximum no. of arguments supported for a command
+#define MAX_LENGTH	1024			//the maximum length of command-line
+
+typedef struct command_queue				//struct to store parsed commands for a line in a linked-list
+{
+	/* data */
+	int curr_arg;							//no. of current arguments
+	char *command[MAX_ARG_NUM+1];			//command[0]=command, rest are arguments for command
+
+} commQ;
+
