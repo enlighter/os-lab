2e23d5c70f89c37ecef5e7475487ef1bc27b46ec
enlighter <enlighter.1992@gmail.com>
2015-02-26 19:44:21 +0530

    add problem statements for assign 3

---
+++ assign3a - advShell/advshell.c
@@ -0,0 +1,366 @@
+/*######### PLEASE INCLUDE THE FILE "myshell.h, parse.c & parse.h" IN THE SAME FOLDER #########*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+//#include <pwd.h>
+//#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <dirent.h>
+#include "myshell.h"
+#include "parse.c"
+/*--------NECESSARY HEADERS INCLUDED---------*/
+
+commQ direction;      //global structure instance for storing parsed command
+
+int main(int argc, char *argv[]) 
+{
+  char line[MAX_LENGTH], cwd[MAX_LENGTH];
+  /* to store command line and to store current directory full path */
+
+  //int command=0;
+  init_commQ(&direction);    //intialize structure
+
+  /* Print the command prompt */
+  system("clear");
+  printf("New custom shell (pid: %d) created:\n\n",getpid());
+  /*----------------------------*/
+
+  /* Enter perpetual loop for main process of shell */
+  while (1) 
+  {
+
+    printf("$ ");
+
+    if(getcwd(cwd, sizeof(cwd)) != NULL)
+    	fprintf(stdout, "%s >", cwd);
+    else
+    	perror("getcwd() error");
+
+    if (!fgets(line, MAX_LENGTH, stdin)) break;
+    //system(line);
+    if (processBuiltInCommand(line) != NO_SUCH_BUILTIN)
+    {
+      //printf("command = %d",command);
+      //executeBuiltInCommand(command);
+    }
+    else
+    {
+      printf("PATH: %s\n", getenv("PATH"));
+      system(line);
+    }
+  }
+  /*----------------------------*/
+
+  free_commQ(&direction);
+  return 0;
+}
+
+int processBuiltInCommand(char * cmd)
+{
+
+  insertToken(cmd, &direction);
+
+
+  if( strncmp(direction.command[0], "exit", strlen("exit")) == 0){
+  
+    if(direction.currArg != 0){
+      printArgumentError();
+      return -1;
+      }
+    return executeExitCommand();
+    }
+  else if( strncmp(cmd, "pwd", strlen("pwd")) == 0){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    if(arg1 != NULL){
+      printArgumentError();
+      return -1;
+      }
+      return executePwdCommand();
+    }
+  else if( strncmp(cmd, "ls", strlen("ls")) == 0){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    if(arg1 != NULL ){
+      if(strncmp(arg1, "-l", strlen("-l")) == 0 ){
+        return executeLsMinusLCommand();
+      }
+      printArgumentError();
+      return -1;
+      }
+
+      return executeLsCommand();
+    }
+  else if( strncmp(cmd, "cd", strlen("cd")) == 0){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    if(arg2 != NULL || arg1 == NULL){
+      printArgumentError();
+      return -1;
+      }
+      return executeCdCommand(arg1);
+    }
+  else if( strncmp(cmd, "cp", strlen("cp")) == 0){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    char * arg3 = strtok (NULL," ");
+    if(arg3 != NULL){
+      printArgumentError();
+      return -1;
+      }
+      return executeCpCommand(arg1,arg2);
+    }
+  else if( strncmp(cmd, "mkdir", strlen("mkdir")) == 0){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    if(arg2 != NULL){
+      printArgumentError();
+      return -1;
+      }
+      return executeMkdirCommand(arg1);
+    }
+  else if( strncmp(cmd, "rmdir", strlen("rmdir")) == 0){
+    char * arg0 = strtok (cmd," ");
+    char * arg1 = strtok (NULL," ");
+    char * arg2 = strtok (NULL," ");
+    if(arg2 != NULL){
+      printArgumentError();
+      return -1;
+      }
+      return executeRmdirCommand(arg1);
+    }
+  else
+    return NO_SUCH_BUILTIN;
+
+  return 0;
+}
+
+int executeCdCommand(char *arg)
+{
+  int status=0;
+  char destination[MAX_LENGTH], currPos[MAX_LENGTH] ;
+
+  if(getcwd(currPos, sizeof(currPos)) == NULL)
+  {
+      perror("getcwd() error");
+  }
+  strcpy(destination, currPos);
+  strcat(destination, "/");
+  strncat(destination, arg, strlen(arg)-1);
+  status = chdir(destination);
+  
+  if(status!=0)
+  {
+    perror("Error");
+    return -1;
+  }
+  else
+    return 0;
+}
+
+int executePwdCommand(){
+  char cwd[MAX_LENGTH];
+  if(getcwd(cwd, sizeof(cwd)) != NULL)
+      printf("Current Working Directory : %s\n", cwd);
+    else{
+      perror("getcwd() error");
+      return -1;
+    }
+  return 0;
+}
+
+int executeMkdirCommand(char * arg){
+
+  int ret=0;
+  char name[20];
+  mode_t process_mask = umask(0);
+
+  strncpy(name, arg, strlen(arg)-1);
+  ret = mkdir(name, S_IRWXU | S_IRWXG | S_IRWXO);
+
+  if(!ret)
+  {
+    printf("Directory %s created\n",name);
+  }
+  else
+  {
+    perror("Error");
+  }
+
+  umask(process_mask);
+  return ret;
+}
+
+int executeRmdirCommand(char * arg){
+
+  int ret=0;
+  char name[20];
+
+  strncpy(name, arg, strlen(arg)-1);
+  ret = rmdir(name);
+
+  if(!ret)
+  {
+    printf("Directory %s deleted\n",name);
+  }
+  else
+  {
+    perror("Delete Error");
+  }
+
+  return ret;
+}
+
+int executeLsCommand(){
+  char cwd[MAX_LENGTH];
+  DIR *dp;
+  dp=opendir(getcwd(cwd, sizeof(cwd)));
+
+  struct dirent *sd;
+  while((sd=readdir(dp))!=NULL)
+  {
+    printf("%s\t",sd->d_name);
+  }
+  printf("\n");
+  closedir(dp);
+  return 0;
+}
+
+int executeLsMinusLCommand(){
+  char cwd[MAX_LENGTH];
+  DIR *dp;
+  dp=opendir(getcwd(cwd, sizeof(cwd))); 
+
+  struct dirent *sd; 
+  struct stat buf;
+  struct passwd *p;
+  struct group *g;
+  struct tm *t;
+
+  char time[26];
+  char P[10]="rwxrwxrwx",AP[10]=" ";
+  int i,j;
+  while((sd=readdir(dp)) != NULL)
+  {
+    stat(sd->d_name,&buf);
+  
+    // File Type
+    if(S_ISDIR(buf.st_mode))
+      printf("d");
+    else if(S_ISREG(buf.st_mode))
+      printf("-");
+    else if(S_ISCHR(buf.st_mode))
+      printf("c");
+    else if(S_ISBLK(buf.st_mode))
+      printf("b");
+    else if(S_ISLNK(buf.st_mode))
+      printf("l");
+    else if(S_ISFIFO(buf.st_mode))
+      printf("p");
+    else if(S_ISSOCK(buf.st_mode))
+      printf("s");
+    //File Permissions P-Full Permissions AP-Actual Permissions
+    for(i=0,j=(1<<8);i<9;i++,j>>=1)
+      AP[i]= (buf.st_mode & j ) ? P[i] : '-' ;
+    printf("%s", AP);
+    //No. of Hard Links
+
+    printf("%5d", (int) buf.st_nlink);
+
+    //User Name
+    p=getpwuid(buf.st_uid);
+    printf(" %.8s", p->pw_name);
+    //Group Name
+    g=getgrgid(buf.st_gid);
+    printf(" %-8.8s", g->gr_name);
+    //File Size
+
+    printf(" %8d", (int) buf.st_size);
+
+    //Date and Time of modification
+    t=localtime(&buf.st_mtime);
+    strftime(time,sizeof(time),"%b %d %H:%M",t);
+    printf(" %s",time);
+    //File Name
+    printf(" %s\n",sd->d_name);
+  }
+  closedir(dp);
+  return 0;
+}
+
+int executeCpCommand(char * arg1, char* arg2){
+  char dest[MAX_LENGTH], currPos[MAX_LENGTH] ;
+  if(getcwd(currPos, sizeof(currPos)) == NULL)
+  {
+      perror("getcwd() error");
+  }
+  strcpy(dest, currPos);
+  strcat(dest, "/");
+  strncat(dest, arg2, strlen(arg2)-1);
+
+  struct stat attr1,attr2;
+  stat(arg1, &attr1);
+  stat(dest, &attr2);
+
+  //printf("Last modified source time: %s", ctime(&attr1.st_ctime));
+  //printf("Last modified target time: %s", ctime(&attr2.st_ctime));
+
+  int timeDiff = difftime(attr2.st_ctime,attr1.st_ctime) > 0 ? 1 : -1;
+
+  FILE *source,*target;
+  char ch;
+  if( access( arg1, F_OK ) == -1 ){
+    printf("File missing !!! \n");
+  }
+  else if( access( arg1, R_OK ) == -1 ){
+    printf("Read Permission, Access Denied !!! \n");
+  }
+
+  source=fopen(arg1,"r");
+  target=fopen(dest,"w");
+  if(source == NULL)
+  {
+    printf("Unable to open source file..ERROR in opening file!!\n");
+    return -1;
+  }
+
+  if( target != NULL && access(  dest, W_OK ) == -1 ){
+    printf("Write Permission, Access Denied !!! \n");
+    return -1;
+  }
+
+  if( target != NULL && timeDiff == 1){
+    printf("Error !!! Destination has a newer file !!! \n");
+    return -1;
+  }
+
+  while((ch=fgetc(source))!=EOF)
+  fputc(ch,target);/*writing to the target file*/
+  printf("Copy Successful !!!\n");
+  fclose(source);
+  fclose(target);
+
+  return 0;
+}
+
+int executeExitCommand(){
+  exit(0);
+  return -1;
+}
+
+void printArgumentError(){
+  printf("Argument Mismatch\n");
+}
---
+++ assign3a - advShell/parse.c
@@ -0,0 +1,112 @@
+/*######### CONTAINS NECESSARY FUNCTIONS FOR PARSING #########*/
+/*######### PLEASE INCLUDE THE FILE "parse.h" IN THE SAME FOLDER #########*/
+/*   Syntax:     command <|arg1> <|arg2> <|arg3> <|arg4> <|arg5>      */
+
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "parse.h"
+
+void init_commQ(commQ *comm)		//initialize structure instance
+{
+	int i=0;
+
+	comm->isEmpty = 1;
+	comm->currArg = 0;
+	for(i = 0; i <= MAX_ARG_NUM; i++)
+	{
+		comm->command[i] = NULL;
+	}
+}
+
+void free_commQ(commQ *comm)		//freeing memory when instance no longer required
+{
+	if(comm->isEmpty)
+		return;
+	else
+	{
+		for(; (comm->currArg) >= 0; (comm->currArg)--)
+		{
+			//printf("Freeing Q%d\n", comm->currArg);
+			free(comm->command[comm->currArg]);
+		}
+	}
+}
+
+void insertToken(char *token, commQ *queue)			//insert elements to the commQ structure
+{
+	//char *temp = NULL;
+	//printf("is empty? %d\n", queue->isEmpty);
+
+	if(queue->isEmpty)
+	{
+		queue->isEmpty = 0;
+		queue->currArg = 0;
+		//printf("Was empty!\n");
+	}
+	else
+	{
+		(queue->currArg)++;
+	}
+
+	queue->command[queue->currArg] = strdup(token);
+
+}
+
+commQ parse(char *line)
+{
+	char token[MAX_LENGTH];
+	commQ ret;
+	int i=0;
+	char *temp = NULL;
+
+	init_commQ(&ret);
+
+	temp = strtok(line, TOKENIZER);
+	//printf("temp: %s\n", temp);
+	//insertToken(temp, &ret);
+
+	for(i=1; i<=MAX_ARG_NUM && temp != NULL; i++)
+	{
+		//printf("temp: %s\n", temp);
+		//printf("is empty? %d\n", ret.isEmpty);
+		insertToken(temp, &ret);
+		//printf("commQ: no.of arguments: %d\n", ret.currArg);
+		temp = strtok(NULL, TOKENIZER);
+	}
+
+	return ret;
+}
+
+/*
+int main()			//for testing purposes
+{
+	commQ comm;
+	char line[MAX_LENGTH];
+	int i=0;
+
+	init_commQ(&comm);
+	//printf("Is empty? %d\n", comm.isEmpty);
+
+	printf("Command input:\n");
+	if (!fgets(line, MAX_LENGTH, stdin))
+	{
+		printf("read error!\n");
+	}
+
+	comm = parse(line);
+	//printf("commQ: no.of arguments: %d\n",comm.currArg);
+	i = 0;
+
+	while(!comm.isEmpty && i<=comm.currArg)
+	{
+		printf("%s\n", comm.command[i]);
+		i++;
+	}
+
+	free_commQ(&comm);
+
+	return 0;
+}
+*/
