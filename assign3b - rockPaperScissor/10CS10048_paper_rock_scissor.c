42499ee305b3f672703d40da07e97dae73137a26
enlighter <enlighter.1992@gmail.com>
2015-02-28 12:39:27 +0530

    ready the pipes for proper execution

--- assign3b - rockPaperScissor/10CS10048_paper_rock_scissor.c
+++ assign3b - rockPaperScissor/10CS10048_paper_rock_scissor.c
@@ -7,9 +7,10 @@
 #define FAULT	-1
 #define BUFSIZE 80	//write lines of 80 chars into the pipe.
 
-int child();
+int participant();	//to be executed by a child for participating in the game
+int mediator();		//to be used by master(host) to mediate the game 
 
-int master()
+int master()		//host of the game
 {
 	int fdc[2], fdd[2];		//pipes for communicating with child c & d respectively
 	char line[BUFSIZE];
@@ -23,33 +24,45 @@ int master()
 	if(!pidC)			//C executing
 	{
 		printf("C is born! [%d]\n", getpid());
+
+		close(fdc[0]); /* The child will not read and
+				     hence we close fdc[0] */
 	}
 	else		//master executing
 	{
 		printf("C created : %d\n", pidC);
 
+		close(fdc[1]); /* The parent will not write and
+				     hence we close fdc[1] */
+
 		pidD = fork();		//fork child D
 		if(!pidD)			//D executing
 		{
 			printf("D is born! [%d]\n", getpid());
+
+			close(fdd[0]); /* The child will not read and
+				     hence we close fdd[0] */
 		}
 		else		//master executing
 		{
 			printf("D created : %d\n", pidD);
+
+			close(fdd[1]); /* The parent will not write and
+				     hence we close fdd[1] */
 		}
 	}
 
 	while(1)
 	{
-		if(!pidC)
+		if(!pidC)		//C executing
 		{
 			printf("C playing game: %d\n", getpid());
 		}
-		else if(!pidD)
+		else if(!pidD)	//D executing
 		{
 			printf("D playing game: %d\n", getpid());
 		}
-		else
+		else			//P (master) executing
 		{
 			printf("Master hosting the game: %d\n", getpid());
 		}
@@ -63,7 +76,12 @@ int main()
 	return (master());
 }
 
-int child()
+int participant()
 {
-	
+
+}
+
+int mediator()
+{
+
 }
