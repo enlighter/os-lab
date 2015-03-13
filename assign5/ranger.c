de51f2bb06cae79b749528c00adffe8083a14deb
enlighter <enlighter.1992@gmail.com>
2015-03-13 22:36:54 +0530

    initialize the programs for lion-jackal problem

---
+++ assign5/ranger.c
@@ -0,0 +1,8 @@
+/* THE PRODUCER - RANGER (Ranger of the national forest) */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <semaphore.h>
