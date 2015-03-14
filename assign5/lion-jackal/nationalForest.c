8ade2533a1b2ee8ba89d5120208a8a77006bbfcb
enlighter <enlighter.1992@gmail.com>
2015-03-15 02:41:10 +0530

    implement user input for process choice

--- assign5/lion-jackal/nationalForest.c
+++ assign5/lion-jackal/nationalForest.c
@@ -17,11 +17,46 @@ standard semaphores	*/
 /*-----------------*/
 #include <semaphore.h>
 
-#include <pit.h>	//Main include header for the whole lion-jackal problem
+#include "pit.h"	//Main include header for the whole lion-jackal problem
 
 int main()
 {
+	short isLion = 0, isJackal = 0, isRanger = 0;	//bool values for which process to run
+	int nJ = 0, nL = 0;		//no of. jackals and lions respectively
+	int choice = 0;		//user choice selection value
+
 	printf("Welcome to the National Forest!!\n");
 
-	ret SUCCESS;
+	while(!choice)
+	{
+		printf("1. Lion\n2. Jackal\n3. Ranger\n");
+		printf("Please choose which process would you like to run currently ? :");
+		scanf("%d", &choice);
+
+		switch(choice)
+		{
+			case 1:
+			{
+				isLion = 1;
+				break;
+			}
+			case 2:
+			{
+				isJackal = 1;
+				break;
+			}
+			case 3:
+			{
+				isRanger = 1;
+				break;
+			}
+			default:
+			{
+				printf("Invalid choice, please enter a valid choice!\n");
+				choice = 0;
+			}
+		}
+	}
+
+	return SUCCESS;
 }
