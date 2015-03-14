a93ad9d85511c7d41b880c19a74fb5dddfb9abc6
enlighter <enlighter.1992@gmail.com>
2015-03-14 23:13:41 +0530

    use UNIX standard for semaphore implementation using semget() etc.

---
+++ assign5/lion-jackal/nationalForest.c
@@ -0,0 +1,27 @@
+/* Welcome to the National Forest in Enlighter World!!
+	Here you'll find lions and jackals
+	and their caretaker, the Ranger!
+
+	Oh, and don't forget to include "pit.h"
+	how else are they gonna eat huh!!
+--------------------------------------------*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/* UNIX based systems' include headers to implement UNIX
+standard semaphores	*/
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+/*-----------------*/
+#include <semaphore.h>
+
+#include <pit.h>	//Main include header for the whole lion-jackal problem
+
+int main()
+{
+	printf("Welcome to the National Forest!!\n");
+
+	ret SUCCESS;
+}
--- assign5/lion-jackal/pit.h
+++ assign5/lion-jackal/pit.h
@@ -3,3 +3,5 @@
 #define	FAULT 			-1	/* return value of a function on faulty execution */
 #define	SUCCESS			0	/* return value of a function on successful execution */
 #define	PIT_CAPACITY	50	/* maximum pit capacity */
+#define NO_OF_PITS		3
+
