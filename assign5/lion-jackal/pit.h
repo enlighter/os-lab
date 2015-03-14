69dfb0177821abc16e26373834b72968d718f7f7
enlighter <enlighter.1992@gmail.com>
2015-03-14 13:31:44 +0530

    create preliminary modular structure of lion-jackal problem

---
+++ assign5/lion-jackal/pit.h
@@ -0,0 +1,5 @@
+/* Main include header for the whole lion-jackal problem */
+
+#define	FAULT 			-1	/* return value of a function on faulty execution */
+#define	SUCCESS			0	/* return value of a function on successful execution */
+#define	PIT_CAPACITY	50	/* maximum pit capacity */
